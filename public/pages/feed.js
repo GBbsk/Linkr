1c04e53521b69e43d267049e88d09549db0e2855
Gabriel Bsk <gabrielbsk21@gmail.com>
2026-08-26 22:31:26 -0300

    feat: Adicionado as funções: Curtir, AtualizarPost e logout ao feed

--- public/pages/feed.js
+++ public/pages/feed.js
@@ -1,6 +1,8 @@
-import { posts } from "../js/api/posts.api.js";
+import { posts, curtirPost } from "../js/api/posts.api.js";
+import { fazerLogout } from "../js/api/auth.api.js";
 
 const infoPainel = document.querySelector('.info-feed');
+
 const template = document.getElementById('template-post');
 const listaPosts = document.querySelector('.post-feed ol');
 
@@ -9,7 +11,7 @@ const initFeed = async () => {
 
     try {
         const dados = await posts();
-        console.log('Posts recebidos:', dados);
+
         infoPainel.innerHTML = ''
 
         const listaDePosts = Array.isArray(dados) ? dados : dados.posts;
@@ -19,7 +21,6 @@ const initFeed = async () => {
         if (infoPainel) infoPainel.innerHTML = 'Erro ao carregar posts.';
     }
 };
-
 
 const renderizarPosts = (posts) => {
     const fragment = document.createDocumentFragment();
@@ -32,6 +33,7 @@ const renderizarPosts = (posts) => {
     posts.forEach(post => {
         const clone = template.content.cloneNode(true)
 
+        clone.querySelector('.post-item').id = post.id;
         clone.querySelector('.titulo-post-feed a').textContent = post.titulo;
         clone.querySelector('.titulo-post-feed a').href = post.url || '#';
         clone.querySelector('.desc-post-feed').textContent = post.descricao
@@ -42,4 +44,91 @@ const renderizarPosts = (posts) => {
     listaPosts.replaceChildren(fragment)
 }
 
-initFeed();
+const curtir = () => {
+    const btnCurtir = document.querySelectorAll('.btn-curtir')
+
+    if (btnCurtir.length === 0) return;
+
+    btnCurtir.forEach((btn) => {
+        btn.addEventListener('click', async (e) => {
+            e.preventDefault()
+
+            const post = btn.closest('.post-item')
+            const id = post ? post.id : null;
+
+            if (!id) { console.warn('ID do post não encontrado.');
+                return;
+            }
+
+            try {
+                const accessToken = sessionStorage.getItem('accessToken')
+
+                if (!accessToken) {
+                    window.location.replace('./login.html')
+                }
+
+                const dados = await curtirPost(id, accessToken)
+                await atualizarPost(dados.id, 'curtir')
+            } catch (error) {
+                console.error('Erro ao curtir post:', error);
+                if(error.message === 'Token inválido ou adulterado.'){
+                    window.location.replace('./login.html')
+                }
+            }
+        })
+    })
+}
+
+const atualizarPost = async (postId, estado = 'curtir') => {
+    const AllPosts = await posts()
+
+    const post = AllPosts.find(p => p.id === postId)
+    const postHTML = document.getElementById(`${post.id}`)
+
+    if(!post){
+        throw new Error('POST_NAO_EXISTE')
+    }
+
+    const btnCurtir = postHTML.querySelector('.btn-curtir');
+    const infoPost = postHTML.querySelector('.info-feed-post');
+
+    if(estado == 'curtir'){
+        btnCurtir.classList.add('btn-curtido')
+        btnCurtir.style.color = '#ffffff'; 
+
+        
+        const textoLikes = post.votos === 1 ? 'like' : 'likes';
+        infoPost.textContent = `${post.votos} ${textoLikes} - @${post.autor}`
+    }
+
+    btnCurtir.style.backgroundColor = '#f9faf9';
+    btnCurtir.style.color = '#1d1c1c'; 
+}
+
+const logout = async () => {
+    const btnLogout = document.querySelector('.logout-head-nav')
+
+    btnLogout.addEventListener('click', async (e) => {
+        e.preventDefault()
+
+    try {
+        await fazerLogout()
+        window.location.replace('./login.html')
+        } catch (error) {
+        console.log('CATCH:', error.message)
+        throw error
+        }
+    })
+}
+
+const iniciar = async () => {
+    await initFeed();    
+    curtir() 
+    logout()
+};
+
+iniciar();
+
+export {
+    curtirPost,
+};

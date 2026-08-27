5fd0a09ee7d37782fc542e00afaad8231d2d0da9
Gabriel Bsk <gabrielbsk21@gmail.com>
2026-08-26 22:27:48 -0300

    chore: Pequenas mudanças para os cookies funcionarem em localhost e em todas as URLs da API

--- src/config/cookies.js
+++ src/config/cookies.js
@@ -2,8 +2,8 @@ const opcoesRefreshCookie = () => {
     return {
         httpOnly: true, // Isso aqui so permite que o token so seja lido durante uma req HTTP
         secure: false, // true: so envia o token se for uma conexao HTTPS
-        path: '/api/v1/auth', 
-        sameSite: 'strict', // isso aqui evita CSRF
+        path: '/', // isso permite que os token sejam enviados para qualquer url da API
+        sameSite: 'lax', // usar strict aqui evita CSRF
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 dias em ms
     }
 }
@@ -12,8 +12,8 @@ const limparRefreshToken = () => {
     return {
         httpOnly: true,
         secure: false,
-        path: '/api/v1/auth',
-        sameSite: 'strict'
+        path: '/',
+        sameSite: 'lax' // usar strict aqui evita CSRF
     }
 }
 
